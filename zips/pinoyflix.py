99da113f8082721c4fbb2688da574785fb005458
pinoytracker <forums.support@gmail.com>
2025-04-08 18:26:09 +0200

    .
    .

--- zips/pinoyflix.py
+++ zips/pinoyflix.py
@@ -104,22 +104,24 @@ def get_p_links(url):
 
 	blocklinks_regex = r'<div class="main-container">(.+?)<span>Leave a Reply</span>'
 	main_block = re.compile(blocklinks_regex,re.DOTALL).findall(str(html))[0]
-	#xbmc.log('Regex_main_block######################################################### '+str(html),2)
+	listmatch_regex = r'<[iI][fF][rR][aA][mM][eE].+?[sS][rR][cC]="(.+?)"'
+	main_listmatch = re.compile(listmatch_regex,re.DOTALL).findall(str(main_block))
+	xbmc.log('Regex_main_block######################################################### '+str(main_listmatch),2)
 
-	if "<strong>Coming Soon</strong>" in main_block:
-		source = '<url>COMING SOON</url>'
-		xbmc.log('SOURCE######################################################### '+str(source),2)
-		#else:
-		#	source = '<url>NO LINKS FOUND</url>'
-		Sources.append(source)
-		#xbmc.log('SOURCES######################################################### '+str(sources),2)
-	else:
-		listmatch_regex = r'<[iI][fF][rR][aA][mM][eE].+?[sS][rR][cC]="(.+?)"'
-		main_listmatch = re.compile(listmatch_regex,re.DOTALL).findall(str(main_block))
-	# 	#xbmc.log('pbin_main_listmatch######################################################### '+str(pbin_main_listmatch),2)
-	# 	#thisRegex =r'<h2 style=\"text-align(.+?)<div id="recent-posts-2'
-	#	Regex_me = re.compile(main_listmatch,re.DOTALL).findall(str(html))
-		xbmc.log('Regex######################################################## '+str(main_listmatch),2)
+	# if "<strong>Coming Soon</strong>" in main_block:
+	# 	source = '<url>COMING SOON</url>'
+	# 	xbmc.log('SOURCE######################################################### '+str(source),2)
+	# 	#else:
+	# 	#	source = '<url>NO LINKS FOUND</url>'
+	# 	Sources.append(source)
+	# 	#xbmc.log('SOURCES######################################################### '+str(sources),2)
+	# else:
+	# 	listmatch_regex = r'<[iI][fF][rR][aA][mM][eE].+?[sS][rR][cC]="(.+?)"'
+	# 	main_listmatch = re.compile(listmatch_regex,re.DOTALL).findall(str(main_block))
+	# # 	#xbmc.log('pbin_main_listmatch######################################################### '+str(pbin_main_listmatch),2)
+	# # 	#thisRegex =r'<h2 style=\"text-align(.+?)<div id="recent-posts-2'
+	# #	Regex_me = re.compile(main_listmatch,re.DOTALL).findall(str(html))
+	# 	xbmc.log('Regex######################################################## '+str(main_listmatch),2)
 
 	#	for link in Regex_me:
 	#		source = '<url>'+link+'</url>'
