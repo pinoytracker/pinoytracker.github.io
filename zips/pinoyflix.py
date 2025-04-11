15b99314209ef533283ec863019406c879b662f6
pinoytracker <forums.support@gmail.com>
2025-04-11 18:24:49 +0200

    .
    .

--- zips/pinoyflix.py
+++ zips/pinoyflix.py
@@ -211,7 +211,25 @@ def scrape_nextpage_pinoysflix(url):
 	return new_data
 
 def scrape_play_vkhost(url):
-	headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/68.0.3440.84 Safari/537.36'}
+	xbmc.log('URL ######################################################### '+str(url),2)
+	headers = {
+		'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7',
+		'accept-language': 'en-US,en;q=0.9,fil;q=0.8,ko;q=0.7',
+		'dnt': '1',
+		'priority': 'u=0, i',
+		'referer': 'https://pinoyflixs.com/',
+		'sec-ch-ua': '"Google Chrome";v="135", "Not-A.Brand";v="8", "Chromium";v="135"',
+		'sec-ch-ua-mobile': '?0',
+		'sec-ch-ua-platform': '"Windows"',
+		'sec-fetch-dest': 'iframe',
+		'sec-fetch-mode': 'navigate',
+		'sec-fetch-site': 'cross-site',
+		'sec-fetch-storage-access': 'none',
+		'upgrade-insecure-requests': '1',
+		'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/135.0.0.0 Safari/537.36',
+	}
+
+
 	Readit = requests.get(url,headers=headers)
 	html = Readit.text
 	xbmc.log('HTML ######################################################### '+str(html),2)
